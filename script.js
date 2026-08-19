42b370b2ffee58094d88c735313a624cfb31973e
Jatin Gupta <animatedworld434@gmail.com>
2026-08-19 18:22:56 +0530

    Changes in index.css

---
+++ script.js
@@ -0,0 +1,24 @@
+var button = document.getElementById("hello")
+var button2 = document.getElementById("hello2")
+var button3 = document.getElementById("btn")
+
+button.addEventListener("contextmenu" , ()=>{
+    alert("DON'T HACK US WE ARE CURRENTLY BROKE...!!!")
+})
+
+button.addEventListener("click" , ()=>{
+    alert("THE SITE IS UNDER CONSTRUCTION...!!!")
+})
+
+
+button2.addEventListener("contextmenu" , ()=>{
+    alert("DON'T HACK US WE ARE CURRENTLY BROKE...!!!")
+})
+
+button3.addEventListener("contextmenu" , ()=>{
+    alert("DON'T HACK US WE ARE CURRENTLY BROKE...!!!")
+})
+
+button3.addEventListener("click" , ()=>{
+    alert("THE SITE IS UNDER CONSTRUCTION...!!")
+})
