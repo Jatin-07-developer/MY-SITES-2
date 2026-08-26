95e9fce816fa2276984ad8e24ed3495a51185572
Jatin Gupta <animatedworld434@gmail.com>
2026-08-26 08:23:00 +0530

    Changes in style.css

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var button = document.getElementById("hello")
 var button2 = document.getElementById("hello2")
 var button3 = document.getElementById("btn")
+var contentSection = document.querySelector("contentSection")
+var scrollBtn = document.getElementById("scrollBtn")
+var contentSection = document.getElementById("contentSection")
+
 
 button.addEventListener("contextmenu" , ()=>{
     alert("DON'T HACK US WE ARE CURRENTLY BROKE...!!!")
@@ -23,9 +27,10 @@ button3.addEventListener("click" , ()=>{
     alert("THE SITE IS UNDER CONSTRUCTION...!!")
 })
 
-var scrollBtn = document.getElementById("scrollBtn")
-var contentSection = document.getElementById("contentSection")
-
 scrollBtn.addEventListener("click", () => {
     contentSection.scrollIntoView({ behavior: "smooth" })
+})
+
+Window.addEventListener("scroll", ()=>{
+
 })
